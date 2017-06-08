494f26fa9fa23ee08e9ec6ea85cbcb9623d7f4e4
GreegAV <greegav@gmail.com>
2017-06-09 01:13:04 +0300

    lab18

--- juja-core/src/ua/com/juja/core/ParserTest2.java
+++ juja-core/src/ua/com/juja/core/ParserTest2.java
@@ -5,10 +5,10 @@ package ua.com.juja.core;
  */
 public class ParserTest2 {
     public static void main(String[] args) {
-        System.out.println(">> 123 = " + Parser.eval("123"));
-        System.out.println(">> 2*3 = " + Parser.eval("2*3"));
-        System.out.println(">> (123+321)-1 = "+Parser.eval("(123+321)-1"));
-        System.out.println(">> (1+3)*2 = " + Parser.eval("(1+3)*2"));
-        System.out.println(">> ((13/6)*2-5)+1 = " + Parser.eval("((13/6)*2-5)+1"));
+        System.out.println(">> 123 = " + Parser_stolen.eval("123"));
+        System.out.println(">> 2*3 = " + Parser_stolen.eval("2*3"));
+        System.out.println(">> (123+321)-1 = "+ Parser_stolen.eval("(123+321)-1"));
+        System.out.println(">> (1+3)*2 = " + Parser_stolen.eval("(1+3)*2"));
+        System.out.println(">> ((13/6)*2-5)+1 = " + Parser_stolen.eval("((13/6)*2-5)+1"));
     }
 }
