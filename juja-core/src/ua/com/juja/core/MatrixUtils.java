a5f9592faeae17e4f90994b4da9c92481f346f3e
GreegAV <greegav@gmail.com>
2017-06-11 22:37:20 +0300

    lab21

--- juja-core/src/ua/com/juja/core/MatrixUtils.java
+++ juja-core/src/ua/com/juja/core/MatrixUtils.java
@@ -11,7 +11,7 @@ public class MatrixUtils {
 
         int[][] result = new int[fst.length][snd[0].length];
 
-        for ( int i = 0; i < result.length;; i++ ) {
+        for ( int i = 0; i < result.length; i++ ) {
             for ( int j = 0; j < result[0].length; j++ ) {
                 for ( int k = 0; k < fst[0].length; k++ ) {
                     result[i][j] += fst[i][k] * snd[k][j];
