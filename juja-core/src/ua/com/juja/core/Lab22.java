05dcdcb7889a5bb1ed55eb275164585c232bd012
GreegAV <greegav@gmail.com>
2017-06-12 15:13:54 +0300

    lab22 rename

--- juja-core/src/ua/com/juja/core/Lab22.java
+++ juja-core/src/ua/com/juja/core/Lab22.java
@@ -11,7 +11,7 @@ package ua.com.juja.core;
  [[1, 2, 3], [4, 5, 6], [7, 8, 9]] -> [[7, 4, 1], [8, 5, 2], [9, 6, 3]] ...
 
  */
-public class ArrayUtils {
+public class Lab22 {
 
     public static void main(String[] argv)
     {
