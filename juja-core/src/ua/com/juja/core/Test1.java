42da1bd374a9caebb7d5c7040e0939297c052510
GreegAV <greegav@gmail.com>
2017-07-02 15:12:52 +0300

    works

---
+++ juja-core/src/ua/com/juja/core/Test1.java
@@ -0,0 +1,12 @@
+package ua.com.juja.core;
+
+/**
+ * Created by avg-m on 02/07/2017.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Test!");
+    }
+
+
+}
